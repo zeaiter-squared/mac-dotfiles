139c3f3cecef295f929c8ca7545d1decf18943ee
Zeaiter Zeaiter <zeaiter.zeaiter@zurich.com>
2025-10-09 21:47:37 +1100

    chore: update debug config

--- config/nvim/lua/zeta-squared/debugging.lua
+++ config/nvim/lua/zeta-squared/debugging.lua
@@ -36,6 +36,18 @@ for _, language in ipairs(languages) do
             runtimeExecutable = "node",
         },
         {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug payload ingestion",
+            cwd = "${workspaceFolder}",
+            runtimeExecutable = "pnpm",
+            runtimeArgs = {"payload", "run", "src/payload/scripts/ingestion", "--", "--refresh"},
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+            sourceMaps = true,
+            skipFiles = {"<node_internals>/**", "node_modules/**"},
+        },
+        {
             name = "Attach to process",
             type = "pwa-node",
             request = "attach",
@@ -61,25 +73,49 @@ for _, language in ipairs(languages) do
     }
 end
 
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+-- dap.listeners.before.attach.dapui_config = function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.launch.dapui_config = function()
+--   dapui.open()
+-- end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited.dapui_config = function()
+--   dapui.close()
+-- end
+
+local cat_config = require('zeta-squared.cat_config')
+local cat_colours = cat_config.color_overrides.macchiato
+vim.highlight['DapBreakpoint'] = {}
+vim.highlight['DapBreakpointCondition'] = {}
+vim.highlight['DapLogPoint'] = {}
+vim.highlight['DapStopped'] = {}
+vim.highlight['DapBreakpointRejected'] = {}
+
+vim.fn.sign_define('DapBreakpoint', {text='', texthl='DapBreakpoint', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointCondition', {text='', texthl='DapBreakpointCondition', linehl='', numhl=''})
+vim.fn.sign_define('DapLogPoint', {text='', texthl='DapLogPoint', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='', texthl='DapStopped', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='DapBreakpointRejected', linehl='', numhl=''})
 
 vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
+vim.keymap.set('n', '<leader>dB',
+    function()
+        local condition = vim.fn.input("Breakpoint condition (optional): ")
+        local hit_condition = vim.fn.input("Hit count (optional): ")
+
+        condition = condition ~= "" and condition or nil
+        hit_condition = hit_condition ~= "" and hit_condition or nil
+        dap.toggle_breakpoint(condition, hit_condition)
+    end
+)
 vim.keymap.set('n', '<leader>dc', dap.continue)
 vim.keymap.set('n', '<leader>dl', dap.step_into)
 vim.keymap.set('n', '<leader>dj', dap.step_over)
 vim.keymap.set('n', '<leader>dh', dap.step_out)
 vim.keymap.set('n', '<leader>dk', dap.step_back)
 vim.keymap.set('n', '<leader>dr', dap.restart)
-vim.keymap.set('n', '<leader>dq', dap.close)
+vim.keymap.set('n', '<leader>dq', dap.terminate)
 vim.keymap.set('n', '<leader>dt', dapui.toggle)
